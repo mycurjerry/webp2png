73f38a1018895c04abd69ea3ae35d2caff136ad8
mycurjerry <143496597+mycurjerry@users.noreply.github.com>
2023-12-30 17:24:17 +0800

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['webpImage'])) {
+        $webpImage = $_FILES['webpImage']['tmp_name'];
+        $pngImage = pathinfo($_FILES['webpImage']['name'], PATHINFO_FILENAME) . '.png';
+
+        $image = imagecreatefromwebp($webpImage);
+        if ($image === false) {
+            http_response_code(500);
+            echo "无法加载 WebP 图片。";
+        } else {
+            header('Content-Type: image/png');
+            imagepng($image, $pngImage);
+            imagedestroy($image);
+            echo "图片转换成功。";
+        }
+    } else {
+        http_response_code(400);
+        echo "没有文件上传。";
+    }
+} else {
+    http_response_code(405);
+    echo "2ui的api，webp转png。只支持 POST 方法。";
+}
+?>
